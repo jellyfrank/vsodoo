274f2112a464f47d857783b848166d6d3dc59d44
block-cat <kfx2007@outlook.com>
2019-05-07 09:18:51 +0000

    test

--- vsodoo/src/extension.ts
+++ vsodoo/src/extension.ts
@@ -1,13 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {Completion} from './odoo';
+import { Completion } from './odoo';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	let log = vscode.window.createOutputChannel("vsodoo/log")
+	let log = vscode.window.createOutputChannel("vsodoo/log");
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	log.show(true);
@@ -20,7 +20,16 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Welcome to use vscode with odoo');
+		
+		// console.log("启动调试...");
+		// let editor = new vscode.WorkspaceEdit();
+		// let uri = new vscode.Uri();
+		// editor.createFile(new vscode.Uri());
+		// vscode.commands.executeCommand("fileExplorer.copy",[".","."]);
+		// vscode.commands.executeCommand("explorer.newFile","test.file");
+
+		vscode.window.showInformationMessage("123");
+
 
 	});
 
@@ -28,9 +37,9 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//add icons
 	let odoobutton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 5);
-	odoobutton.text = `$(plug)`
-	odoobutton.tooltip = "odoo extension ready."
-	odoobutton.show()
+	odoobutton.text = `$(plug)`;
+	odoobutton.tooltip = "odoo extension ready.";
+	odoobutton.show();
 
 	//register completion
 	// vscode.languages.registerCompletionItemProvider("python",new Completion());
